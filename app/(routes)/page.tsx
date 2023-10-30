abbbf0691392da8d6481305b4183592b06377ae9
Bilgekaan36 <b.yilmaz@workdigital.de>
2023-10-30 15:15:29 +0300

    [BY-1] add getBillboard and implement Billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,12 +1,16 @@
+import getBillboard from "@/actions/get-billboard";
 import { Billboard } from "@/components/billboard";
 import Container from "@/components/ui/container";
-import React from "react";
+
+export const revalidate = 0;
 
 const HomePage = async () => {
+  const data = await getBillboard("b5c2dd5c-12b0-4232-a174-09a3013c2d39");
+
   return (
     <Container>
       <div className='space-y-10 pb-10'>
-        <Billboard />
+        <Billboard data={data} />
       </div>
     </Container>
   );
